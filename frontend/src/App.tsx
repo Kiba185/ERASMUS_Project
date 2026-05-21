d9dd6165f1ba4bf7c800cf4c68be95427dcf69bf
Filip Vápeník <filiphorse.a@gmail.com>
2026-05-21 13:38:08 +0200

    commit books and header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import Header from "./components/header";
+import Books from "./components/books";
+
 function App() {
+  const [value, setValue] = useState(0);
+  function handleClick()
+  {
+    setValue(value + 1);
+    console.log(value);
+  }
   return (
     <div className="flex items-center justify-center min-h-screen bg-slate-900 text-white">
-      <h1 className="text-3xl font-bold text-amber-500">Tailwind úspěšně nasazen!</h1>
+      <Header/>
+      <h1 className="text-3xl font-bold text-amber-500">ENGINEERS{value}</h1>
+      <h2 className="text-3xl font-bold text-amber-500">Coming soon...</h2>
+      <button onClick={handleClick}>+1</button>
+      <Books />
+
     </div>
   )
 }
---
+++ frontend/src/components/books.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+const mockBooks = [
+  { id: 1, title: "Buy groceries", price: 10.99 },
+  { id: 2, title: "Walk the dog", price: 5.99 },
+  { id: 3, title: "Read a book", price: 15.99 },
+  { id: 4, title: "Go to the gym", price: 20.00 },
+];
+
+function Books() {
+    const [books, setBooks] = useState(mockBooks);
+    return (
+
+        <div className="flex items-center justify-center min-h-screen bg-slate-900 text-white">
+            {books.map((book) => (
+                <div key={book.id} className="bg-slate-700 p-4 rounded-lg m-2">
+                    <h2 className="text-xl font-bold">{book.title}</h2>
+                    <p className="text-lg font-semibold">${book.price}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+export default Books;
