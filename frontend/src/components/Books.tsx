bb2362d6fc1baa8a5f23483c0fcbcf85843a34e3
Richard Vávra <risavavra@gmail.com>
2026-05-21 13:52:37 +0200

    headerBooks

--- frontend/src/components/Books.tsx
+++ frontend/src/components/Books.tsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 const mockBooks = [
-  { id: 1, title: "Buy groceries", price: 1000 },
-  { id: 2, title: "Walk the dog", price: 1000},
-  { id: 3, title: "Read a book", price: 1000 },
-  {id: 4, title: "Jo", price: 10000}
+  { id: 1, title: "Buy groceries", price: 10.99 },
+  { id: 2, title: "Walk the dog", price: 5.99 },
+  { id: 3, title: "Read a book", price: 15.99 },
+  { id: 4, title: "Go to the gym", price: 20.00 },
 ];
 
-function Books(){
-    const [books, setBooks] = useState(mockBooks)
-    return <div>
-        <ul>
-        {books.map(book => (
-          <li key={book.id}>{book.title}: {book.price}</li>
-        ))}
-      </ul>
+function Books() {
+    const [books, setBooks] = useState(mockBooks);
+    return (
 
-    </div>
+        <div className="flex items-center justify-center min-h-screen bg-slate-900 text-white">
+            {books.map((book) => (
+                <div key={book.id} className="bg-slate-700 p-4 rounded-lg m-2">
+                    <h2 className="text-xl font-bold">{book.title}</h2>
+                    <p className="text-lg font-semibold">${book.price}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
 export default Books;
