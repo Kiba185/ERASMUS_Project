86c445b7c9cb617c4854bc14a2eae260eff7482b
Filip Vápeník <filiphorse.a@gmail.com>
2026-05-21 13:50:28 +0200

    books a header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,8 +1,8 @@
-function Header() {
+function Header(){
+    
     return (
-        <header className="bg-slate-900 text-white p-4 margin-top-0">
-            <h1 className="text-2xl font-bold">ENGINEERS-HEADER</h1>
-        </header>
+        <div><h1>KOKOR</h1></div>
+
     );
 }
 export default Header;
