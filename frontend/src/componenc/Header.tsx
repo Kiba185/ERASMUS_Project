5d69c7f9420d4f18eb1e16dc77887ef8d96a2d49
Samuel080410 <samuel.sticha8@gmail.com>
2026-05-25 10:42:47 +0200

    summary

---
+++ frontend/src/componenc/Header.tsx
@@ -0,0 +1,9 @@
+Function Header() {
+  return (
+    <header className="bg-slate-800 text-white py-4">
+      <h1 className="text-2xl font-bold">Moje aplikace</h1>
+    </header>
+  );
+}
+
+export default Header;
