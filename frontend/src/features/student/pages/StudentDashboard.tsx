dc57336a7a39425a1db725ef2100fcf3cbf17c5f
Jakub Malota <jakub.malota08@gmail.com>
2026-05-25 11:30:28 +0200

    Books exercise

--- frontend/src/features/student/pages/StudentDashboard.tsx
+++ frontend/src/features/student/pages/StudentDashboard.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Books from "../../../components/books";
+import Header from "../../../components/header";
+
+
 
 const StudentDashboard: React.FC = () => {
-  return <h1>Student Area</h1>;
+  return <div>
+    <Header />
+    <Books />
+  </div>;
 };
 
 export default StudentDashboard;
