71a7a8a17b0a6a3f2e1e9b4f901017b0347a4715
Richard Vávra <risavavra@gmail.com>
2026-05-26 11:56:52 +0200

    Routes

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import ScheduleWidget from '../components/widgets/Schedulewidget';
+import ScheduleWidget from '../components/widgets/ScheduleWidget';
 import GradesWidget from '../components/widgets/GradesWidget';
 
 
