39bffeac514d7b0eac8d7ee43fc67efcdca458f7
Domzdr <domzdr@seznam.cz>
2026-05-29 12:09:10 +0200

    jo

---
+++ frontend/src/pages/AbsencePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AbsencePage: React.FC = () => {
+    return (
+        <div className="p-8">
+            <h1 className="text-2xl font-bold">Absence overview for student</h1>
+            <h1 className="text-4xl font-bold">Domča je černý</h1>
+        </div>
+    );
+};
+export default AbsencePage;
